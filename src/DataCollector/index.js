f1b45d2a32b914f5c8dc27fef804fc14343d4af7
Gaeta <dj@gaeta.me>
2022-04-24 18:54:49 -0400

    Push changes to logger.

--- src/DataCollector/index.js
+++ src/DataCollector/index.js
@@ -31,39 +31,38 @@ FuelRewardsAPI.on('ready', async () => {
 });
 
 async function run() {
-        const Mysql = new MysqlWrapper(require('../../config/mysql'));
-        console.log("Fuel Rewards API is Ready, Starting Collection...");
-        let locations = await FuelRewardsAPI.getStations(CollectionConfig.getStations);
-    
-        if (locations?.length <= 0 || !locations) return console.log("No Locations Found");
-    
-        for (i = 0; i < locations.length; i++) {
-            const station = locations[i];
-            console.log(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Starting`);
-            
-            // Check to see if the station is already in the database if not create it.
-            if (!await Mysql.getStation(station)) throw new Error(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Station Not Found`);
-            
-            for (f = 0; f < station.fuelDetails.length; f++) {
-                new Promise(async (resolve, reject) => {
-                    const fuel = station.fuelDetails[f];
-                    if (!fuel.retailFuelPrice) fuel.retailFuelPrice = "0.00";
-                    console.log(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Adding Fuel Record for ${fuel.longDescription} @ ${fuel.retailFuelPrice}`);
-    
-                    const fuelType = await Mysql.getFuelType(fuel);
-                    if (!fuelType) throw new Error(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Fuel Type Not Found`);
+    const Mysql = new MysqlWrapper(require('../../config/mysql'));
+    console.log("Fuel Rewards API is Ready, Starting Collection...");
+    let locations = await FuelRewardsAPI.getStations(CollectionConfig.getStations);
 
-                    // Add Fuel Record - Yes IK its a promise in a ansyc shh
-                    Mysql.query("INSERT INTO fuel_prices (stationId, type, price, excludedRewardAmount, redeemableRewardAmount, date_reported) VALUES (?, ?, ?, ?, ?, ?)", [station.id, fuelType.id, fuel.retailFuelPrice, station.excludedRewardAmount, station.redeemableRewardAmount, station.datePriceReported])
-                    .then((r) => {
-                        console.log(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Added Fuel Record for ${fuel.longDescription} @ ${fuel.retailFuelPrice}`);
-                        resolve();
-                    }).catch((err) => {
-                        console.error(err);
-                        reject(err);
-                    });
-                });
+    if (locations?.length <= 0 || !locations) return console.log("No Locations Found");
+
+    for (i = 0; i < locations.length; i++) {
+        const station = locations[i];
+        console.log(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Starting`);
+        
+        // Check to see if the station is already in the database if not create it.
+        if (!await Mysql.getStation(station)) throw new Error(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Station Not Found`);
+        
+        for (f = 0; f < station.fuelDetails.length; f++) {
+            const fuel = station.fuelDetails[f];
+            if (!fuel.retailFuelPrice) fuel.retailFuelPrice = "0.00";
+            console.log(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Adding Fuel Record for ${fuel.longDescription} @ ${fuel.retailFuelPrice}`);
+
+            const fuelType = await Mysql.getFuelType(fuel);
+            if (!fuelType) throw new Error(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Fuel Type Not Found`);
+
+            try {
+                Mysql.query("INSERT INTO fuel_prices (stationId, type, price, excludedRewardAmount, redeemableRewardAmount, date_reported) VALUES (?, ?, ?, ?, ?, ?)", [station.id, fuelType.id, fuel.retailFuelPrice, station.excludedRewardAmount, station.redeemableRewardAmount, station.datePriceReported])
+                console.log(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Added Fuel Record for ${fuel.longDescription} @ ${fuel.retailFuelPrice}`);
+            } catch (e) {
+                console.error(e);
             }
+            console.log(`${i + 1}/${locations.length + 1} - ${station.name}(${station.id}): Finished`);
         }
-        if (reRun.amount) console.log("Fuel Recorder Finished, Running Again in " + (reRun.amount / 1000 / 60) + " mins");
+    }
+    if (reRun.amount) console.log("Fuel Recorder Finished, Running Again in " + (reRun.amount / 1000 / 60) + " mins");
+    else console.log("Fuel Recorder Finished, Running Once");
+    // Close the mysql connection
+    await Mysql.close();
 }
